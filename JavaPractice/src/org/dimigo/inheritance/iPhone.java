3d8bfd5db37fbd5cc4c515f0a379a31da0333398
hd132507 <kwchany@gmail.com>
2015-08-25 10:30:35 +0900

    SmartPhone Test

---
+++ JavaPractice/src/org/dimigo/inheritance/iPhone.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * |_iPhone
+	1. 개요 : 
+	2. 작성일 :  2015. 8. 25.
+	
+ * @author : 김의찬
+	@version : 1.0
+ *
+ * 
+ */
+public class iPhone extends SmartPhone {
+	iPhone()
+	{
+		
+	}
+	
+	iPhone(String model, String company, int price)
+	{
+		super(model,company,price);
+	}
+	
+	public void useAirDrop()
+	{
+		System.out.println("Airdrop 기능을 사용합니다.");
+	}
+}
